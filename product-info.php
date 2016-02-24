643b91230770933e40bfcfc9c3fc85d069ac03da
Klaus Heino <klaus.heino@gmail.com>
2016-02-24 18:17:21 +0200

    Title has been replaced.

--- product-info.php
+++ product-info.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>e-rauta</title>
+		<title>uGamer Store</title>
 		<link rel="stylesheet" href="CSS/styles.css" type="text/css"/> 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="refresh" content="1200; url=https://users.metropolia.fi/~klaush/VerkkosovellustenOhjelmointi/Projekti/product-info.php"> 
